c61509ba77c75529f3c334a3a3b36e61b4019b31
root <dl57934@naver.com>
2018-04-19 15:27:59 +0000

    create new Docker image

--- ch4/lang-plot.py
+++ ch4/lang-plot.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import pandas as pd
-import json 
-
+import json
 with open('./lang/freq.json',"r",encoding= 'utf-8') as fp:
     freq = fp.read()
 
