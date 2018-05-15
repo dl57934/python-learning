64f51c45871e4c03532274fdd04c2c20847d29ee
root <dl57934@naver.com>
2018-05-15 18:48:41 +0000

    lstm too difficult ㅠㅠ 살려줘

--- ch6/ch6-6/lstm-generation.py
+++ ch6/ch6-6/lstm-generation.py
@@ -4,13 +4,18 @@ from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
+from konlpy.tag import Twitter
+from bs4 import BeautifulSoup
 import numpy as np
 import random
 import sys
 import io
-
-path = "./lstm-generation.py"
-fp = open(path, encoding='utf-8') 
+import codecs
+path = "./BEXX0003.txt"
+fp = codecs.open(path, 'r',encoding='utf-16')
+bsData = BeautifulSoup(fp,'html.parser')
+body = bsData.select_one('body')
+text = body.getText()+" "
 print('corpus length:', len(text))
 
 chars = sorted(list(set(text)))
@@ -18,8 +23,9 @@ print('total chars:', len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
+
 # cut the text in semi-redundant sequences of maxlen characters
-maxlen = 40
+maxlen = 20
 step = 3
 sentences = []
 next_chars = []
@@ -57,41 +63,26 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
-
-def on_epoch_end(epoch, logs):
-    # Function invoked at end of each epoch. Prints generated text.
-    print()
-    print('----- Generating text after Epoch: %d' % epoch)
-
-    start_index = random.randint(0, len(text) - maxlen - 1)
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
-        print('----- diversity:', diversity)
-
+for iteration in range(1,60):
+    print("반복 = ",iteration)
+    model.fit(x, y,batch_size=128,nb_epoch=1)
+    start_index = random.randint(0,len(text) - maxlen - 1)
+    for diversity in [0.2,0.5,1.0,1.2]:
+        print('---다양성= ',diversity)
         generated = ''
-        sentence = text[start_index: start_index + maxlen]
+        sentence = text[start_index,strat_index+maxlen]
         generated += sentence
-        print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
-
         for i in range(400):
-            x_pred = np.zeros((1, maxlen, len(chars)))
+            x = np.zeros((1,maxlen,len(chars)))
             for t, char in enumerate(sentence):
-                x_pred[0, t, char_indices[char]] = 1.
-
-            preds = model.predict(x_pred, verbose=0)[0]
+                x [0,t,char_indices[char]] = 1
+            preds = model.predict(x,verbose=0)[0]
             next_index = sample(preds, diversity)
             next_char = indices_char[next_index]
-
             generated += next_char
             sentence = sentence[1:] + next_char
-
             sys.stdout.write(next_char)
             sys.stdout.flush()
-        print()
 
-print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 
-model.fit(x, y,
-          batch_size=128,
-          epochs=60,
-          callbacks=[print_callback])
